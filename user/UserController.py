8753dc5a34362b3c75563a7b96b585553b7d82c8
lemon37564 <65079334+lemon37564@users.noreply.github.com>
2022-05-02 22:30:41 +0800

    Update UserController.py

--- user/UserController.py
+++ user/UserController.py
@@ -73,7 +73,7 @@ def CheckUser(token, random) -> "flask.Response":
         })
 
 
-def LoginUser():
+def LoginUser(account: str, password: str):
     """login = (redirect_to聊天頁面) ? (有該帳號存在且經過驗證) : (重新導引至登入頁面並依狀況顯示其相應行為)"""
     return ""
 
@@ -88,7 +88,7 @@ def EditUser():
     return ""
 
 
-def DeleteUser():
+def DeleteUser(account: str, password: str):
     """Delete = (切至登入頁面) ? (成功刪除帳號) : (失敗?，可能為Bug，need修復)"""
     return ""
 
