c74e3d519aadac8cd67e903ae3a35fe023f6643b
wc2014920 <58461634+wc2014920@users.noreply.github.com>
2022-05-05 14:32:27 +0800

    修改結構(大)

--- app/run.py
+++ app/run.py
@@ -1,4 +1,12 @@
 from app import app
+from flask import url_for
+from werkzeug.utils import redirect
+
+
+@app.route('/')
+def startpoint():
+    return redirect(url_for('UserRoutes.start'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
