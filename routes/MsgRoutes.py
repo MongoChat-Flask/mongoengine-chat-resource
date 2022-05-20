228ff297c85da6a31afe77408f6216633626a161
lemon37564 <65079334+lemon37564@users.noreply.github.com>
2022-05-20 17:22:33 +0800

    Update MsgRoutes.py

--- routes/MsgRoutes.py
+++ routes/MsgRoutes.py
@@ -7,3 +7,9 @@ MsgRoutes = Blueprint('MsgRoutes', __name__)
 @MsgRoutes.route('/send', methods=['POST'])
 def send():
     return "send_message()"
+
+
+@MsgRoutes.route('/get', methods=['GET'])
+def get():
+    # must follow json format
+    return '{"msgs": ["hi", "hello", "how", "he", "hand", "hover", "human"]}'
