701544f8674d80d5ef39251841a9e7fd5afbd92f
wc2014920 <58461634+wc2014920@users.noreply.github.com>
2022-08-04 20:18:16 +0800

    Deploy

--- routes/static/js/socketio.js
+++ routes/static/js/socketio.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Connect to websocket location.protocol
     //var socket = io.connect('http://' + document.domain + ':' + location.port);
     var protocol = window.location.protocol;
-    var socket = io.connect(protocol + '//' + document.domain + ':' + location.port + namespace);
+    var socket = io.connect(protocol + '//' + document.domain + ':' + location.port + '/chat-r/index');
     // Set default room
     let room = "global"
 
