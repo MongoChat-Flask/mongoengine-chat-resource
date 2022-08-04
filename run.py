1d02d40058aa9a0478c5ff4edf23024b935ea732
wc2014920 <58461634+wc2014920@users.noreply.github.com>
2022-08-04 15:07:40 +0800

    Deploy

--- run.py
+++ run.py
@@ -58,4 +58,5 @@ def join(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, port=int(os.environ.get('PORT', '5000')), debug=True)
+    # socketio.run(app, port=int(os.environ.get('PORT', '5000')), debug=True)
+    socketio.run(app, debug=True)
